02f0427da44e9e4e60a85d5698190e6dd17c7579
Narayami <tfamsoad@gmail.com>
2019-01-23 15:17:35 +0000

    removed a folder

--- InterviewExercices/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        WordCounter wordCounter = new WordCounter("ola ola");
-        System.out.println(wordCounter.startCharCount());
-
-        System.out.println(wordCounter.startWordCount());
-
-    }
-}
