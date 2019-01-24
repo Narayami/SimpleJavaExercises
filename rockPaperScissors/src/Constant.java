ac903e6cb199bbb87f057bab0eb8ed9ffc81ce88
Narayami <tfamsoad@gmail.com>
2019-01-24 17:33:26 +0000

    Added Constant Class

---
+++ rockPaperScissors/src/Constant.java
@@ -0,0 +1,6 @@
+public final class Constant {
+    public final static String PLAYERONE = "Player One: ";
+    public final static String PLAYERTWO = "Player Two: ";
+    public final static String PLAYED = " Played: ";
+
+}
--- rockPaperScissors/src/Game.java
+++ rockPaperScissors/src/Game.java
@@ -33,20 +33,20 @@ public class Game {
     }
 
     private String getScissorsResult(Hand p1Hand, Hand p2Hand) {
-        System.out.println("P1 - " + playerOne + " played " + p1Hand);
-        System.out.println("P2 - " + playerTwo + " played " + p2Hand);
+        System.out.println(Constant.PLAYERONE + playerOne + " played " + p1Hand);
+        System.out.println(Constant.PLAYERTWO + playerTwo + " played " + p2Hand);
         return p2Hand == Hand.PAPER ? "P1 Wins" : p2Hand == Hand.ROCK ? "P2 Wins" : "Draw";
     }
 
     private String getPaperResult(Hand p1Hand, Hand p2Hand) {
-        System.out.println("P1 - " + playerOne + " played " + p1Hand);
-        System.out.println("P2 - " + playerTwo + " played " + p2Hand);
+        System.out.println(Constant.PLAYERONE + playerOne + " played " + p1Hand);
+        System.out.println(Constant.PLAYERTWO + playerTwo + " played " + p2Hand);
         return p2Hand == Hand.ROCK ? "P1 Wins" : p2Hand == Hand.SCISSORS ? "P2 Wins" : "Draw";
     }
 
     private String getRockResult(Hand p1Hand, Hand p2Hand) {
-        System.out.println("P1 - " + playerOne + " played " + p1Hand);
-        System.out.println("P2 - " + playerTwo + " played " + p2Hand);
+        System.out.println(Constant.PLAYERONE + playerOne + " played " + p1Hand);
+        System.out.println(Constant.PLAYERTWO + playerTwo + " played " + p2Hand);
         return p2Hand == Hand.SCISSORS ? "P1 Wins" : p2Hand == Hand.PAPER ? "P2 Wins" : "Draw";
     }
 
